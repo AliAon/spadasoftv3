21ed57428fa6cc7d236ff9bfdfa497d560fe4235
Aon Mohammad <aon094944@gmail.com>
2023-03-08 13:34:32 +0500

    Career links updated

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -1,32 +1,32 @@
 export const DummyTeamData = [
     {
       name: "Jonas Adam",
-      picture_path: "images/team/Image (16).png",
+      picture_path: "./images/team/Image (16).png",
       position: "Co-Founder & Developer",
     },
     {
       name: "Jonas Adam",
-      picture_path: "images/team/Image (17).png",
+      picture_path: "./images/team/Image (17).png",
       position: "Co-Founder & Developer",
     },
     {
       name: "Jonas Adam",
-      picture_path: "images/team/Image (18).png",
+      picture_path: "./images/team/Image (18).png",
       position: "Co-Founder & Developer",
     },
     {
       name: "Jonas Adam",
-      picture_path: "images/team/Image (19).png",
+      picture_path: "./images/team/Image (19).png",
       position: "Co-Founder & Developer",
     },
     {
       name: "Jonas Adam",
-      picture_path: "images/team/Image (16).png",
+      picture_path: "./images/team/Image (16).png",
       position: "Co-Founder & Developer",
     },
     {
       name: "Jonas Adam",
-      picture_path: "images/team/Image (17).png",
+      picture_path: "./images/team/Image (17).png",
       position: "Co-Founder & Developer",
     },
   ];
